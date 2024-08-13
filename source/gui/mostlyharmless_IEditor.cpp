40aab18e1fe7b2b5ffb55fbfa07b51aec81d5ba4
MeijisIrlnd <syl.morrison.dev@gmail.com>
2024-08-13 16:05:41 +0100

    more functional style with EditorContext

--- source/gui/mostlyharmless_IEditor.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Syl on 12/08/2024.
-//
-#include <mostly_harmless/gui/mostlyharmless_IEditor.h>
-namespace mostly_harmless::gui {
-    IEditor::IEditor(mostly_harmless::gui::EditorContext context) : m_context(context) {
-
-    }
-}
