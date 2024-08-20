f52917bb1dde10ab7b727eea48373e5911cf94ef
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-08-20 00:29:35 +0000

    deploy: 72ec741ce6e6680b9f5d9130b36ee49affdb206b

--- search/namespaces_0.js
+++ search/namespaces_0.js
@@ -15,9 +15,10 @@ var searchData=
   ['marvin_3a_3amath_3a_3awindows_12',['windows',['../namespacemarvin_1_1math_1_1windows.html',1,'marvin::math']]],
   ['marvin_3a_3autils_13',['utils',['../namespacemarvin_1_1utils.html',1,'marvin']]],
   ['mostly_5fharmless_14',['mostly_harmless',['../namespacemostly__harmless.html',1,'']]],
-  ['mostly_5fharmless_3a_3aevents_15',['events',['../namespacemostly__harmless_1_1events.html',1,'mostly_harmless']]],
-  ['mostly_5fharmless_3a_3agui_16',['gui',['../namespacemostly__harmless_1_1gui.html',1,'mostly_harmless']]],
-  ['mostly_5fharmless_3a_3agui_3a_3ahelpers_17',['helpers',['../namespacemostly__harmless_1_1gui_1_1helpers.html',1,'mostly_harmless::gui']]],
-  ['mostly_5fharmless_3a_3agui_3a_3ahelpers_3a_3amacos_18',['macos',['../namespacemostly__harmless_1_1gui_1_1helpers_1_1macos.html',1,'mostly_harmless::gui::helpers']]],
-  ['mostly_5fharmless_3a_3autils_19',['utils',['../namespacemostly__harmless_1_1utils.html',1,'mostly_harmless']]]
+  ['mostly_5fharmless_3a_3aaudio_15',['audio',['../namespacemostly__harmless_1_1audio.html',1,'mostly_harmless']]],
+  ['mostly_5fharmless_3a_3aevents_16',['events',['../namespacemostly__harmless_1_1events.html',1,'mostly_harmless']]],
+  ['mostly_5fharmless_3a_3agui_17',['gui',['../namespacemostly__harmless_1_1gui.html',1,'mostly_harmless']]],
+  ['mostly_5fharmless_3a_3agui_3a_3ahelpers_18',['helpers',['../namespacemostly__harmless_1_1gui_1_1helpers.html',1,'mostly_harmless::gui']]],
+  ['mostly_5fharmless_3a_3agui_3a_3ahelpers_3a_3amacos_19',['macos',['../namespacemostly__harmless_1_1gui_1_1helpers_1_1macos.html',1,'mostly_harmless::gui::helpers']]],
+  ['mostly_5fharmless_3a_3autils_20',['utils',['../namespacemostly__harmless_1_1utils.html',1,'mostly_harmless']]]
 ];
