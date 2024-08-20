eff56b6571eb2a6b1aeeb51dcf0c9fd6c79fd384
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-08-20 00:25:50 +0000

    deploy: a8ecfb5b2a1771e53c6a705016b910ad0977c2f1

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['hamming_0',['hamming',['../namespacemarvin_1_1math_1_1windows.html#a7be1a2f895faae28c033eabac1835b95',1,'marvin::math::windows']]],
-  ['handlenoteoff_1',['handleNoteOff',['../classmostly__harmless_1_1Plugin.html#a850e13473b306db92b9ed7799f92fbf7',1,'mostly_harmless::Plugin']]],
-  ['handlenoteon_2',['handleNoteOn',['../classmostly__harmless_1_1Plugin.html#ac57c1551038a2c3b29e92088a8b7d4c5',1,'mostly_harmless::Plugin']]],
-  ['hann_3',['hann',['../namespacemarvin_1_1math_1_1windows.html#a8b0f4e506555cbf059716b84966c599d',1,'marvin::math::windows']]],
-  ['hide_4',['hide',['../structmostly__harmless_1_1gui_1_1IEditor.html#ab104e5c92f4d37e09f7866a0083dcbcb',1,'mostly_harmless::gui::IEditor::hide()'],['../classmostly__harmless_1_1gui_1_1WebviewEditor.html#a66967191f5c07628faad58b680698962',1,'mostly_harmless::gui::WebviewEditor::hide()']]],
-  ['hideview_5',['hideView',['../namespacemostly__harmless_1_1gui_1_1helpers_1_1macos.html#a8ae69eea9aecfc8ac734d56213687048',1,'mostly_harmless::gui::helpers::macos']]],
-  ['highpass_6',['highpass',['../namespacemarvin_1_1dsp_1_1filters_1_1rbj.html#a524c1e3b92f0ce3057830a6b22f48d6f',1,'marvin::dsp::filters::rbj']]],
-  ['highshelf_7',['highShelf',['../namespacemarvin_1_1dsp_1_1filters_1_1rbj.html#a709f224e22e7115b8e801501c6e38776',1,'marvin::dsp::filters::rbj']]]
+  ['handleevent_1',['handleEvent',['../classmostly__harmless_1_1Plugin.html#a1c61b977581cedcc41e4861df7a07310',1,'mostly_harmless::Plugin']]],
+  ['handlenoteoff_2',['handleNoteOff',['../classmostly__harmless_1_1Plugin.html#a850e13473b306db92b9ed7799f92fbf7',1,'mostly_harmless::Plugin']]],
+  ['handlenoteon_3',['handleNoteOn',['../classmostly__harmless_1_1Plugin.html#ac57c1551038a2c3b29e92088a8b7d4c5',1,'mostly_harmless::Plugin']]],
+  ['hann_4',['hann',['../namespacemarvin_1_1math_1_1windows.html#a8b0f4e506555cbf059716b84966c599d',1,'marvin::math::windows']]],
+  ['hide_5',['hide',['../structmostly__harmless_1_1gui_1_1IEditor.html#ab104e5c92f4d37e09f7866a0083dcbcb',1,'mostly_harmless::gui::IEditor::hide()'],['../classmostly__harmless_1_1gui_1_1WebviewEditor.html#a66967191f5c07628faad58b680698962',1,'mostly_harmless::gui::WebviewEditor::hide()']]],
+  ['hideview_6',['hideView',['../namespacemostly__harmless_1_1gui_1_1helpers_1_1macos.html#a8ae69eea9aecfc8ac734d56213687048',1,'mostly_harmless::gui::helpers::macos']]],
+  ['highpass_7',['highpass',['../namespacemarvin_1_1dsp_1_1filters_1_1rbj.html#a524c1e3b92f0ce3057830a6b22f48d6f',1,'marvin::dsp::filters::rbj']]],
+  ['highshelf_8',['highShelf',['../namespacemarvin_1_1dsp_1_1filters_1_1rbj.html#a709f224e22e7115b8e801501c6e38776',1,'marvin::dsp::filters::rbj']]]
 ];
