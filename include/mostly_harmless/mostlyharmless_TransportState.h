7aacf96101cc96cc3ad2d112f8d2a63bd8808d0d
MeijisIrlnd <syl.morrison.dev@gmail.com>
2024-09-26 00:48:45 +0100

    documented TransportState

--- include/mostly_harmless/mostlyharmless_TransportState.h
+++ include/mostly_harmless/mostlyharmless_TransportState.h
@@ -8,30 +8,91 @@
 #include <optional>
 namespace mostly_harmless {
 
+    /**
+     * \brief Convenience struct holding the numerator and denominator of a time signature. Two ints in a trenchcoat.
+     *
+     */
     struct TimeSignature {
+        /**
+         * The numerator of the time signature (the "4" in 4/4)
+         */
         std::uint16_t numerator;
+        /**
+         * The denominator of the time signature (the other "4" in 4/4)
+         */
         std::uint16_t denominator;
     };
 
+    /**
+     * \brief Convenience struct holding relevant positional info about a loop the host may currently be running.
+     */
     struct LoopInfo {
-        std::int64_t loopStartBeats;
-        std::int64_t loopEndBeats;
-        std::int64_t loopStartSeconds;
-        std::int64_t loopEndSeconds;
+        /**
+         * The position of the start of the loop, in beats.
+         */
+        double startBeats;
+        /**
+         * The position of the end of the loop, in beats.
+         */
+        double endBeats;
+        /**
+         * The position of the start of the loop, in seconds.
+         */
+        double startSeconds;
+        /**
+         * The position of the end of the loop, in seconds.
+         */
+        double endSeconds;
     };
 
+    /**
+     * \brief Contains info from the host about the current transport state.
+     *
+     * Note that the host isn't guaranteed to provide all of, or even any of this information - which is why most of the members are optional.
+     * Make sure you check they're valid before blindly dereferencing them!
+     */
     struct TransportState final {
+        /**
+         * @private
+         */
         explicit TransportState(const clap_event_transport_t* transportEvent);
 
+        /**
+         * Contains the current bpm of the host, or std::nullopt.
+         */
         std::optional<double> bpm{};
-        std::optional<std::int64_t> positionBeats{};
-        std::optional<std::int64_t> positionSeconds{};
+        /**
+         * Contains the current playback position in beats, or std::nullopt.
+         */
+        std::optional<double> positionBeats{};
+        /**
+         * Contains the current playback position in seconds, or std::nullopt.
+         */
+        std::optional<double> positionSeconds{};
+        /**
+         * Contains the current time signature, or std::nullopt.
+         */
         std::optional<TimeSignature> timeSignature{};
-        bool isPlaying;
-        bool isRecording;
-        bool isLoopActive;
+        /**
+         * true if the host is currently playing, false otherwise.
+         */
+        bool isPlaying{ false };
+        /**
+         * true if the host is currently recording, false otherwise.
+         */
+        bool isRecording{ false };
+        /**
+         * true if the host is currently looping, false otherwise. If this is true, then (hopefully), loopInfo is not a nullopt..
+         */
+        bool isLoopActive{ false };
+        /**
+         * Contains positional info about the currently active loop (if isLoopActive), or nullopt.
+         */
         std::optional<LoopInfo> loopInfo;
-        bool isWithinPreroll;
+        /**
+         * true if the host is currently in a "preroll" (count-in).
+         */
+        bool isWithinPreroll{ false };
     };
 
 } // namespace mostly_harmless
--- source/mostlyharmless_TransportState.cpp
+++ source/mostlyharmless_TransportState.cpp
@@ -3,16 +3,24 @@
 //
 #include <mostly_harmless/mostlyharmless_TransportState.h>
 namespace mostly_harmless {
+    [[nodiscard]] double beattimeToDouble(clap_beattime t) {
+        return static_cast<double>(t) / static_cast<double>(CLAP_BEATTIME_FACTOR);
+    }
+
+    [[nodiscard]] double sectimeToDouble(clap_sectime t) {
+        return static_cast<double>(t) / static_cast<double>(CLAP_SECTIME_FACTOR);
+    }
+
     TransportState::TransportState(const clap_event_transport_t* transportEvent) {
         const auto flags = transportEvent->flags;
         if (const auto hasBpm = flags & CLAP_TRANSPORT_HAS_TEMPO; hasBpm) {
             bpm = transportEvent->tempo;
         }
-        if (const auto hasTimeBeats = flags & CLAP_TRANSPORT_HAS_BEATS_TIMELINE; hasTimeBeats) {
-            positionBeats = transportEvent->song_pos_beats;
+        if (auto hasTimeBeats = flags & CLAP_TRANSPORT_HAS_BEATS_TIMELINE) {
+            positionBeats = beattimeToDouble(transportEvent->song_pos_beats);
         }
-        if (const auto hasTimeSeconds = flags & CLAP_TRANSPORT_HAS_SECONDS_TIMELINE; hasTimeSeconds) {
-            positionSeconds = transportEvent->song_pos_seconds;
+        if (auto hasTimeSeconds = flags & CLAP_TRANSPORT_HAS_SECONDS_TIMELINE) {
+            positionSeconds = sectimeToDouble(transportEvent->song_pos_seconds);
         }
         if (const auto hasTimeSignature = flags & CLAP_TRANSPORT_HAS_TIME_SIGNATURE; hasTimeSignature) {
             timeSignature = { .numerator = transportEvent->tsig_num, .denominator = transportEvent->tsig_denom };
@@ -21,10 +29,10 @@ namespace mostly_harmless {
         isRecording = flags & CLAP_TRANSPORT_IS_RECORDING;
         if (isLoopActive = flags & CLAP_TRANSPORT_IS_LOOP_ACTIVE; isLoopActive) {
             loopInfo = {
-                .loopStartBeats = transportEvent->loop_start_beats,
-                .loopEndBeats = transportEvent->loop_end_beats,
-                .loopStartSeconds = transportEvent->loop_start_seconds,
-                .loopEndSeconds = transportEvent->loop_end_seconds
+                .startBeats = beattimeToDouble(transportEvent->loop_start_beats),
+                .endBeats = beattimeToDouble(transportEvent->loop_end_beats),
+                .startSeconds = sectimeToDouble(transportEvent->loop_start_seconds),
+                .endSeconds = sectimeToDouble(transportEvent->loop_end_seconds)
             };
         }
         isWithinPreroll = flags & CLAP_TRANSPORT_IS_WITHIN_PRE_ROLL;
