0ced185b890e49cefbe715b4f60a191c62dd54ac
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-10-13 18:11:48 +0000

    deploy: d662d18b788ecc79c8986d10f04d7ad78f149993

--- search/all_15.js
+++ search/all_15.js
@@ -5,7 +5,7 @@ var searchData=
   ['_7elatticeapf_2',['~LatticeAPF',['../classmarvin_1_1dsp_1_1filters_1_1LatticeAPF.html#a51ba10ffb356a6fd3c5fff8908db18fd',1,'marvin::dsp::filters::LatticeAPF']]],
   ['_7enoiseoscillator_3',['~NoiseOscillator',['../classmarvin_1_1dsp_1_1oscillators_1_1NoiseOscillator.html#a13a82fe53fce1ed144943145ae925907',1,'marvin::dsp::oscillators::NoiseOscillator']]],
   ['_7eoscillatorbase_4',['~OscillatorBase',['../classmarvin_1_1dsp_1_1oscillators_1_1OscillatorBase.html#a68f3f714f51d447872f30aaa6b6356d0',1,'marvin::dsp::oscillators::OscillatorBase']]],
-  ['_7eplugin_5',['~Plugin',['../classmostly__harmless_1_1Plugin.html#a3ef8af0e6428160331a1bac46f712b69',1,'mostly_harmless::Plugin']]],
+  ['_7eplugin_5',['~Plugin',['../classmostly__harmless_1_1Plugin.html#a7d030d76bb5a6ade0dc2d6e99faf7f80',1,'mostly_harmless::Plugin']]],
   ['_7epulseoscillator_6',['~PulseOscillator',['../classmarvin_1_1dsp_1_1oscillators_1_1PulseOscillator.html#ae85bb28bfa45ccef21f572baad46d535',1,'marvin::dsp::oscillators::PulseOscillator']]],
   ['_7esawoscillator_7',['~SawOscillator',['../classmarvin_1_1dsp_1_1oscillators_1_1SawOscillator.html#af4bbf616af5bc98327b15b7a10836023',1,'marvin::dsp::oscillators::SawOscillator']]],
   ['_7esineoscillator_8',['~SineOscillator',['../classmarvin_1_1dsp_1_1oscillators_1_1SineOscillator.html#ab23dae7fbc55fe9da8e26eada295adf4',1,'marvin::dsp::oscillators::SineOscillator']]],
