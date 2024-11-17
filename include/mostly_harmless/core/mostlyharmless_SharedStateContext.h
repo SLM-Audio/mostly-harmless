044a66eccc22d42c73f2d3e2dc8e4886d8905442
MeijisIrlnd <syl.morrison.dev@gmail.com>
2024-11-17 14:16:38 +0000

    Documented SharedStateContext

--- include/mostly_harmless/core/mostlyharmless_SharedStateContext.h
+++ include/mostly_harmless/core/mostlyharmless_SharedStateContext.h
@@ -6,8 +6,19 @@
 #define MOSTLYHARMLESS_MOSTLYHARMLESS_SHAREDSTATECONTEXT_H
 #include <functional>
 namespace mostly_harmless::core {
+    /**
+     * \brief Container for the context required by ISharedState's constructor.
+     */
     struct SharedStateContext {
+
+        /**
+         * A lambda wrapping a call to the PluginBase's runOnMainThread function.
+         */
         std::function<void(std::function<void(void)>&&)> runOnMainThread{ nullptr };
+
+        /**
+         * A lambda wrapping a call to the PluginBase's requestParamFlush function.
+         */
         std::function<void(void)> requestParamFlush{ nullptr };
     };
 } // namespace mostly_harmless::core
